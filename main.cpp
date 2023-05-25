037f61859b59d6fdcd2778ea30e6a5d344cf3625
yang <yangcheng@zuzuche.com>
2023-05-25 18:28:07 +0800

    update.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-#include "yolov5-seg/yolov5_seg_ncnn.cpp"
+//#include "yolov5-seg/yolov5_seg_ncnn.cpp"
+#include "yolov8-seg/yolov8_seg_ncnn.cpp"
 
 int main() {
-    test_yolov5_seg_ncnn();
+//    test_yolov5_seg_ncnn();
+    test_yolov8_seg_ncnn();
 
     return 0;
 }
