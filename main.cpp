946359a437e7c96479f930bcb800b68654aefbb1
yang <yangcheng@zuzuche.com>
2023-05-30 18:44:22 +0800

    调试yolov8-pose.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 //#include "yolov5-seg/yolov5_seg_ncnn.cpp"
-#include "yolov8-seg/yolov8_seg_ncnn.cpp"
+//#include "yolov8-seg/yolov8_seg_ncnn.cpp"
+#include "yolov8-pose/yolov8_pose_ncnn.cpp"
 
 int main() {
 //    test_yolov5_seg_ncnn();
-    test_yolov8_seg_ncnn();
+//    test_yolov8_seg_ncnn();
+    test_yolov8_pose_ncnn();
 
     return 0;
 }
