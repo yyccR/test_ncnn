109398fe67c86ba1870b949db5b0be8e34f9f8c7
yang <yangcheng@zuzuche.com>
2023-06-02 19:30:33 +0800

    update yolov8-pose.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 
 //#include "yolov5-seg/yolov5_seg_ncnn.cpp"
 //#include "yolov8-seg/yolov8_seg_ncnn.cpp"
-#include "yolov8-pose/yolov8_pose_ncnn.cpp"
+//#include "yolov8-pose/yolov8_pose_ncnn.cpp"
 //#include "yolov8/yolov8_ncnn.cpp"
+#include "yolo-nas/yolo_nas_ncnn.cpp"
 
 int main() {
 //    test_yolov5_seg_ncnn();
 //    test_yolov8_seg_ncnn();
-    test_yolov8_pose_ncnn();
+//    test_yolov8_pose_ncnn();
 //    test_yolov8_ncnn();
+    test_yolo_nas_ncnn();
 
     return 0;
 }
