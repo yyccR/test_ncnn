042c3f18cf6ea1811867ed78df57b4984b9575ed
yang <yangcheng@zuzuche.com>
2023-11-05 16:59:17 +0800

    add sherpa param/bin.

--- sherpa/sherpa_ncnn.cpp
+++ sherpa/sherpa_ncnn.cpp
@@ -1,6 +1,126 @@
 
+#include <iostream>
+#include <string>
+#include "net.h"
 #include "c-api/c-api.h"
 
 void test_sherpa_ncnn() {
+
+    std::string encode_param_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/encoder_jit_trace-pnnx.ncnn.param");
+    std::string encode_bin_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/encoder_jit_trace-pnnx.ncnn.bin");
+    std::string decode_param_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/decoder_jit_trace-pnnx.ncnn.param");
+    std::string decode_bin_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/decoder_jit_trace-pnnx.ncnn.bin");
+    std::string joiner_param_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/joiner_jit_trace-pnnx.ncnn.param");
+    std::string joiner_bin_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/joiner_jit_trace-pnnx.ncnn.bin");
+    std::string tokens_file("/Users/yang/CLionProjects/test_ncnn2/sherpa/tokens.txt");
+
+    SherpaNcnnRecognizerConfig config;
+    memset(&config, 0, sizeof(config));
+
+    config.model_config.tokens = tokens_file.c_str();
+    config.model_config.encoder_param = encode_param_file.c_str();
+    config.model_config.encoder_bin = argv[3];
+    config.model_config.decoder_param = argv[4];
+    config.model_config.decoder_bin = argv[5];
+    config.model_config.joiner_param = argv[6];
+    config.model_config.joiner_bin = argv[7];
+
+//    int32_t num_threads = 4;
+//    if (argc >= 10 && atoi(argv[9]) > 0) {
+//        num_threads = atoi(argv[9]);
+//    }
+//
+//    config.model_config.num_threads = num_threads;
+//    config.model_config.use_vulkan_compute = 0;
+//
+//    config.decoder_config.decoding_method = "greedy_search";
+//
+//    if (argc >= 11) {
+//        config.decoder_config.decoding_method = argv[10];
+//    }
+//
+//    config.decoder_config.num_active_paths = 4;
+//    config.enable_endpoint = 0;
+//    config.rule1_min_trailing_silence = 2.4;
+//    config.rule2_min_trailing_silence = 1.2;
+//    config.rule3_min_utterance_length = 300;
+
+//    config.feat_config.sampling_rate = 16000;
+//    config.feat_config.feature_dim = 80;
+//    if (argc >= 12) {
+//        config.hotwords_file = argv[11];
+//    }
+//
+//    if (argc == 13) {
+//        config.hotwords_score = atof(argv[12]);
+//    }
+//
+//    SherpaNcnnRecognizer *recognizer = CreateRecognizer(&config);
+//
+//    const char *wav_filename = argv[8];
+//    FILE *fp = fopen(wav_filename, "rb");
+//    if (!fp) {
+//        fprintf(stderr, "Failed to open %s\n", wav_filename);
+//        return -1;
+//    }
+//
+//    // Assume the wave header occupies 44 bytes.
+//    fseek(fp, 44, SEEK_SET);
+//
+//    // simulate streaming
+//
+//#define N 3200  // 0.2 s. Sample rate is fixed to 16 kHz
+//
+//    int16_t buffer[N];
+//    float samples[N];
+//    SherpaNcnnStream *s = CreateStream(recognizer);
+//
+//    SherpaNcnnDisplay *display = CreateDisplay(50);
+//    int32_t segment_id = -1;
+//
+//    while (!feof(fp)) {
+//        size_t n = fread((void *)buffer, sizeof(int16_t), N, fp);
+//        if (n > 0) {
+//            for (size_t i = 0; i != n; ++i) {
+//                samples[i] = buffer[i] / 32768.;
+//            }
+//            AcceptWaveform(s, 16000, samples, n);
+//            while (IsReady(recognizer, s)) {
+//                Decode(recognizer, s);
+//            }
+//
+//            SherpaNcnnResult *r = GetResult(recognizer, s);
+//            if (strlen(r->text)) {
+//                SherpaNcnnPrint(display, segment_id, r->text);
+//            }
+//            DestroyResult(r);
+//        }
+//    }
+//    fclose(fp);
+//
+//    // add some tail padding
+//    float tail_paddings[4800] = {0};  // 0.3 seconds at 16 kHz sample rate
+//    AcceptWaveform(s, 16000, tail_paddings, 4800);
+//
+//    InputFinished(s);
+//
+//    while (IsReady(recognizer, s)) {
+//        Decode(recognizer, s);
+//    }
+//    SherpaNcnnResult *r = GetResult(recognizer, s);
+//    if (strlen(r->text)) {
+//        SherpaNcnnPrint(display, segment_id, r->text);
+//    }
+//
+//    DestroyResult(r);
+//
+//    DestroyDisplay(display);
+//
+//    DestroyStream(s);
+//    DestroyRecognizer(recognizer);
+//
+//    fprintf(stderr, "\n");
+//
+//    return 0;
 
 }
